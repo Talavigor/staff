2828cecc2ad9f90482a89dc05981278b420302a8
Talavigor <talaviraigor@gmail.com>
2019-11-11 01:52:44 +0200

    Delete staff_password_resets.sql

--- dump_mysql/staff_password_resets.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE staff.password_resets
-(
-    email varchar(255) NOT NULL,
-    token varchar(255) NOT NULL,
-    created_at timestamp
-);
-CREATE INDEX password_resets_email_index ON staff.password_resets (email);
